634f01512ca85fb51510581572f2ebbd96c60100
moamen2020 <m01144944981@gmail.com>
2020-10-27 03:37:02 +0200

    Updates

---
+++ precache-manifest.f6b25c4a770ff01e5e58b37fb0077530.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bf38db503e0a8de4d12bfcf3547b2257",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "b106f155dea3b65280a0",
+    "url": "/portfolio/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "5a2b0c8dfcba1ebdfac7",
+    "url": "/portfolio/static/css/main.a21b14da.chunk.css"
+  },
+  {
+    "revision": "b106f155dea3b65280a0",
+    "url": "/portfolio/static/js/2.dbdf29b5.chunk.js"
+  },
+  {
+    "revision": "c87550333da8df8ab4c80bd1d6e9defb",
+    "url": "/portfolio/static/js/2.dbdf29b5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5a2b0c8dfcba1ebdfac7",
+    "url": "/portfolio/static/js/main.95925423.chunk.js"
+  },
+  {
+    "revision": "88fbd3d397ae3690d5f2",
+    "url": "/portfolio/static/js/runtime-main.fb1b3dac.js"
+  },
+  {
+    "revision": "93adf289b6effdb39305e9d244d98ef1",
+    "url": "/portfolio/static/media/IMG.93adf289.jpg"
+  }
+]);
