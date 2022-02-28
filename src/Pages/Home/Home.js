28d98cefb5207eb97af602b9c2414e8a5b25f951
moamen2020 <m01144944981@gmail.com>
2022-03-01 01:56:09 +0200

    Add Home Page && css

--- src/Pages/Home/Home.js
+++ src/Pages/Home/Home.js
@@ -1,7 +1,36 @@
-import React from 'react'
+import React from "react";
+import { motion } from "framer-motion";
+import './Home.css'
 
-export default function Home() {
+const Home = () => {
   return (
-    <div>Home</div>
-  )
-}
+    <div className="container">
+      <div className="home ">
+        <motion.h2
+          initial={{ x: "-100vw" }}
+          animate={{ x: 0 }}
+          transition={{ delay: 1 }}
+        >
+          I'm <span>Moamen Omar</span>
+        </motion.h2>
+        <motion.span
+          className="name-job"
+          initial={{ x: "100vw" }}
+          animate={{ x: 0 }}
+          transition={{ delay: 1 }}
+        >
+          Front End Developer
+        </motion.span>
+        <motion.button
+          initial={{ opacity: 0 }}
+          animate={{ opacity: 1 }}
+          transition={{ delay: 1.5 }}
+        >
+          <a href='#' target="_blank" rel="noopener noreferrer">Downloud CV</a> 
+        </motion.button>
+      </div>
+    </div>
+  );
+};
+
+export default Home;
