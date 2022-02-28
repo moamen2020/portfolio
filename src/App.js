1b1f85f07fe902d39fddbe8626efaa69b038db22
moamen2020 <m01144944981@gmail.com>
2022-03-01 01:50:25 +0200

    Add Navbar

--- src/App.js
+++ src/App.js
@@ -1,10 +1,26 @@
-import React from "react";
+import React from 'react';
+import { Routes ,Route } from "react-router-dom";
+import Navbar from './components/Navbar/Navbar'
+import Home from './Pages/Home/Home'
+import About from './Pages/About/About'
+import Skills from './Pages/Skills/Skills'
+import Projects from './Pages/Projects/Projects'
+import Contact from './Pages/Contact/Contact'
+
 function App() {
   return (
-    <div className="App">
-      <h1>Portfolio</h1>
+    <div className='app'>
+        <Navbar />
+        <Routes>
+          <Route exact path="/" element={<Home />} />
+          <Route path="about" element={<About />} />
+          <Route path="about" element={<Skills />} />
+          <Route path="about" element={<Projects />} />
+          <Route path="about" element={<Contact />} />
+        </Routes>
     </div>
-  );
-}
-
-export default App;
+    );
+  }
+  
+  export default App;
+  
---
+++ src/components/Navbar/Navbar.js
@@ -0,0 +1,46 @@
+import React from "react";
+import { Link } from "react-router-dom";
+import { motion } from "framer-motion";
+import "./Navbar.css"
+
+const Navbar = () => {
+  return (
+    <>
+      <motion.nav
+        initial={{ y: -250 }}
+        animate={{ y: -10 }}
+        transition={{ delay: 0.2, type: "spring", stiffness: 120 }}
+      >
+        <ul className="list">
+          <li className="nav-links">
+            <Link className="nav-ancor" to="/">
+              Home
+            </Link>
+          </li>
+          <li className="nav-links">
+            <Link className="nav-ancor" to="/about">
+              About
+            </Link>
+          </li>
+          <li className="nav-links">
+            <Link className="nav-ancor" to="/skills">
+              Skills
+            </Link>
+          </li>
+          <li className="nav-links">
+            <Link className="nav-ancor" to="/projects">
+              Projects
+            </Link>
+          </li>
+          <li className="nav-links">
+            <Link className="nav-ancor" to="/contact">
+              Contact
+            </Link>
+          </li>
+        </ul>
+      </motion.nav>
+    </>
+  );
+};
+
+export default Navbar;
