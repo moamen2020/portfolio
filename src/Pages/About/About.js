905b23dd874042195244404c93a67d41826ca786
moamen2020 <m01144944981@gmail.com>
2022-03-01 13:35:27 +0200

    Add Bootstrap-5

--- src/Pages/About/About.js
+++ src/Pages/About/About.js
@@ -2,35 +2,43 @@ import React from "react";
 import img from "../../assets/img.svg";
 import { motion } from "framer-motion";
 import './About.css'
+import { Container, Row, Col } from 'react-bootstrap'
+import "bootstrap/dist/css/bootstrap.min.css";
 
 const About = () => {
   return (
-    <div className="container">
-      <motion.div 
-        className="image"
-        initial={{ x: "-100vw" }}
-        animate={{ x: 0 }}
-        transition={{ delay: 0.5, type: "spring", stiffness: 50 }}
-      >
-        <img src={img} alt="img" />
-      </motion.div>
-      <motion.div 
-        className="info"
-        initial={{ x: "100vw" }}
-        animate={{ x: 0 }}
-        transition={{ delay: 0.5, type: "spring", stiffness: 50 }}
-        >
-          <h2>I am <span>Moamen Omar</span></h2>
-          <p>
+    <Container>
+      <Row className="justify-content-md-center">
+        <Col sm={12} md={6} >
+          <motion.div
+            className="image"
+            initial={{ x: "-100vw" }}
+            animate={{ x: 0 }}
+            transition={{ delay: 0.5, type: "spring", stiffness: 50 }}
+          >
+            <img src={img} alt="img" />
+          </motion.div>
+        </Col>
+        <Col sm={12} md={6}>
+          <motion.div
+            class="information"
+            initial={{ x: "100vw" }}
+            animate={{ x: 0 }}
+            transition={{ delay: 0.5, type: "spring", stiffness: 50 }}
+          >
+            <h2>
+              I am <span>Moamen Omar</span>
+            </h2>
+            <p>
               I am a frontend web developer. I can provide clean code and <br />
               pixel prefect design. I also make website more & more <br />
               interactive with web animation
-          </p>
-          <span class="inf">
+            </p>
+            <span class="inf">
               <strong class="str-1">Full Name </strong> : Moamen Omar
             </span>
             <span class="inf">
-              <strong class="str-2">Age </strong> : 21 Years
+              <strong class="str-2">Age </strong> : 20 Years
             </span>
             <span class="inf">
               <strong class="str-3">Nationality </strong> : Egypt
@@ -51,8 +59,10 @@ const About = () => {
             <span class="inf">
               <strong class="str-8">Phone </strong> : 011 449 449 81
             </span>
-      </motion.div>
-    </div>
+          </motion.div>
+        </Col>
+      </Row>
+    </Container>
   );
 };
 
