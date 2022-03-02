f0d035d8fb1c0848df03aee55b31f437c0cd12d0
moamen2020 <m01144944981@gmail.com>
2022-03-02 02:48:21 +0200

    Section contact

--- src/Pages/Contact/Contact.js
+++ src/Pages/Contact/Contact.js
@@ -1,7 +1,53 @@
-import React from 'react'
+import React from "react";
+import { motion } from "framer-motion";
+import { Container } from 'react-bootstrap'
+import "bootstrap/dist/css/bootstrap.min.css";
+import './Contact.css';
 
-export default function Contact() {
+const Contact = () => {
   return (
-    <div>Contact</div>
-  )
-}
+    <div className="contact">
+      <Container>
+        <motion.h2
+          initial={{ y: "-100vw" }}
+          animate={{ y: 0 }}
+          transition={{ delay: 0.7, type: "spring", stiffness: 10 }}
+        >
+          Contact
+        </motion.h2>
+        <motion.input
+          type="text"
+          placeholder="Your Name"
+          initial={{ x: "-100vw" }}
+          animate={{ x: 0 }}
+          transition={{ delay: 1, type: "spring", stiffness: 80 }}
+        />
+        <motion.input
+          type="email"
+          placeholder="Your Email"
+          initial={{ x: "-100vw" }}
+          animate={{ x: 0 }}
+          transition={{ delay: 1.3, type: "spring", stiffness: 80 }}
+        />
+        <motion.textarea
+          cols="65"
+          rows="15"
+          placeholder="Message"
+          initial={{ x: "-100vw" }}
+          animate={{ x: 0 }}
+          transition={{ delay: 1.6, type: "spring", stiffness: 80 }}
+        ></motion.textarea>
+        <motion.button
+          className="btn-send-Message"
+          initial={{ x: "-100vw" }}
+          animate={{ x: 0 }}
+          transition={{ delay: 1.9, type: "spring", stiffness: 80 }}
+        >
+          Send Message
+        </motion.button>
+      </Container>
+    </div>
+  );
+};
+
+export default Contact;
